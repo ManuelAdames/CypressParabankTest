85e86347706c7e2f55006767410ad36dd69c3369
ManuelAdames <73320573+ManuelAdames@users.noreply.github.com>
2023-05-22 21:00:56 -0400

    Delete Test cypress directory

--- Test cypress/cypress/e2e/Test/functions/landpage.js
+++
@@ -1,8 +0,0 @@
-export class Landpage {
-
-    GoToLandpage(){
-
-        cy.visit('https://parabank.parasoft.com/parabank/index.htm')
-        cy.xpath("//p[contains(.,'Experience the difference')]").should('be.visible')
-    }
-}
